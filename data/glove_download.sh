ad797c22a516920a26c52f3e2c5f4f01fe1c8d18
codefetish <vishaalmohan@gmail.com>
2016-02-20 16:03:17 -0600

    scripts to get download word vectors

---
+++ data/glove_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# To download the glove word vectors
+
+curl http://nlp.stanford.edu/data/glove.6B.zip > glove.zip
+
+unzip \*.zip
---
+++ src/get_started.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+chmod +x ../data/glove_download.sh
+
+cd ../data/
+./glove_download.sh
