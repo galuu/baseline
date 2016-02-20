bc900df56727f39b6043afc82fb80567996ec51b
codefetish <vishaalmohan@gmail.com>
2016-02-20 16:21:20 -0600

    Small changes

--- data/glove_download.sh
+++ data/glove_download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # To download the glove word vectors
 
+mkdir glove
+cd glove
 curl http://nlp.stanford.edu/data/glove.6B.zip > glove.zip
 
 unzip \*.zip
--- src/get_started.sh
+++ src/get_started.sh
@@ -2,4 +2,6 @@
 chmod +x ../data/glove_download.sh
 
 cd ../data/
+mkdir glove
+cd glove
 ./glove_download.sh
