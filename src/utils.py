ccb25212e067aa91e4b3ffd4801221009f4f9977
codefetish <vishaalmohan@gmail.com>
2016-02-20 15:29:08 -0600

    adding utils with word vectors reader

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+import operator
+import json
+import scipy.io as sio
+
+def readGloveData(glove_word_vec_file):
+    f = open(glove_word_vec_file, 'r')
+    rawData = f.readlines()
+    word_vec_dict = {}
+    for line in rawData:
+        line = line.strip().split()
+        tag = line[0]
+        vec = line[1:]
+        word_vec_dict[tag] = np.array(vec, dtype=float)
+    return word_vec_dict
+
+def getWordVector(word, word_vec_dict):
+	if word in word_vec_dict:
+		return word_vec_dict[word]
+	return np.zeros_like(word_vec_dict['hi'])
+
+
+
+def main():
+    glove_word_vec_file = './../glove/glove.6B.300d.txt'
+    word_vec_dict = readGloveData(glove_word_vec_file)
+
+if __name__ == "__main__":
+    main()
